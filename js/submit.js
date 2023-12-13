6025080f98ee480d76ced996b87f8a49a4c7b154
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-12-12 23:54:02 -0500

    fix: Profile picture template

--- js/submit.js
+++ js/submit.js
@@ -16,68 +16,56 @@ async function submit() {
     document.getElementById("user-id").innerHTML = "User ID: " + id;
     document.getElementById("user-username").innerHTML =
       "Username: " + username;
-      document.getElementById("user-discrim").innerHTML = "Discriminator: " + discrim
-      document.getElementById("user-flags").innerHTML = "Flags:" + flags.join(",")
+    document.getElementById("user-discrim").innerHTML =
+      "Discriminator: " + discrim;
+    document.getElementById("user-flags").innerHTML =
+      "Flags:" + flags.join(",");
     document.getElementById("user-banner-color").innerHTML =
       "Banner Color: " + bannerColor;
     document.getElementById("user-accent-color").innerHTML =
       "Accent Color: " + accentColor;
-
-    switch (status) {
-      case "online":
-        document.getElementById("status").src = "../assets/online.png";
-        break;
-      case "idle":
-        document.getElementById("status").src = "../assets/idle.png";
-        break;
-      case "dnd":
-        document.getElementById("status").src = "../assets/dnd.png";
-        break;
-      case "streaming":
-        document.getElementById("status").src = "../assets/streaming.png";
-        break;
-      default:
-        document.getElementById("status").src = "../assets/offline.png";
-    }
+    status
+      ? (document.getElementById("status").src = `../assets/${status}.png`)
+      : (document.getElementById("status").src = "../assets/offline.png");
 
     if (res.presence.activities) {
-        var activity = res.presence.activities[0].name;
-        var type = res.presence.activities[0].type;
-        var typeString;
-        switch (type) {
-          case 0:
-            typeString = "playing";
-            break;
-          case 1:
-            typeString = "streaming";
-            break;
-          case 2:
-            typeString = "listening to";
-            break;
-          case 3:
-            typeString = "watching";
-            break;
-          case 4:
-            typeString = "playing";
-            break;
-          case 5:
-            typeString = "competing";
-            break;
-          default: {
-            typeString = "not playing anything";
-          }
+      var activity = res.presence.activities[0].name;
+      var type = res.presence.activities[0].type;
+      var typeString;
+      switch (type) {
+        case 0:
+          typeString = "playing";
+          break;
+        case 1:
+          typeString = "streaming";
+          break;
+        case 2:
+          typeString = "listening to";
+          break;
+        case 3:
+          typeString = "watching";
+          break;
+        case 4:
+          typeString = "playing";
+          break;
+        case 5:
+          typeString = "competing";
+          break;
+        default: {
+          typeString = "not playing anything";
         }
-        document.getElementById("presence").innerHTML = "Presence: " +typeString + " " + activity;
-      } else {
-        document.getElementById("presence").innerHTML = "Presence: not playing anything";
-        return;
       }
+      document.getElementById("presence").innerHTML =
+        "Presence: " + typeString + " " + activity;
+    } else {
+      document.getElementById("presence").innerHTML =
+        "Presence: not playing anything";
+      return;
+    }
   } else {
-    alert("You must provide a Discord user id.")
+    alert("You must provide a Discord user id.");
   }
 }
 
-
 // soon
-function parseFlags(...flags) {
-}
+function parseFlags(...flags) {}
